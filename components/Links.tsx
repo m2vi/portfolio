48873d0e7a5b065756063bff8ede977505496c9f
m2v <69117810+iuvoJS@users.noreply.github.com>
2021-10-10 22:45:44 +0200

    wat?

--- components/Links.tsx
+++ components/Links.tsx
@@ -4,16 +4,16 @@ const Links = ({ me }) => {
   return (
     <div className='grid grid-cols-4 place-items-center w-full px-4 text-accent'>
       <a href={`https://discord.com/users/${me.discord}`} title='Discord'>
-        <FaDiscord className='w-5 h-5 cursor-pointer' />
+        <FaDiscord className='w-5 h-5 cursor-pointer hover:text-accent-hover' />
       </a>
       <a href={`https://github.com/${me.github}`} title='GitHub'>
-        <FaGithubAlt className='w-5 h-5 cursor-pointer' />
+        <FaGithubAlt className='w-5 h-5 cursor-pointer hover:text-accent-hover' />
       </a>
       <a href={`https://steamcommunity.com/profiles/${me.steam}`} title='Steam'>
-        <FaSteam className='w-5 h-5 cursor-pointer' />
+        <FaSteam className='w-5 h-5 cursor-pointer hover:text-accent-hover' />
       </a>
       <a href={`https://open.spotify.com/user/${me.spotify}`} title='Spotify'>
-        <FaSpotify className='w-5 h-5 cursor-pointer' />
+        <FaSpotify className='w-5 h-5 cursor-pointer hover:text-accent-hover' />
       </a>
     </div>
   );
